da031963efd32a137a0ef14200329e5c9221b773
Leslie <lesbellamy99@gmail.com>
2024-04-09 23:47:47 -0400

    update

---
+++ example/example.js
@@ -0,0 +1,14 @@
+import { four_two_three_one, four_three_three } from '/Formations/Formations.js'
+
+const fourtwo = document.querySelector('.four-two')
+const fourthree = document.querySelector('.four-three')
+
+fourtwo.addEventListener('click', () => {
+    four_two_three_one('ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo',)
+});
+
+fourthree.addEventListener('click', () => {
+    four_three_three('ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo', 'ronaldo',)
+})
+
+
